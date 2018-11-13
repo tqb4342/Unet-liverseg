d030aa81d420281e7d7f6d1115e2e59fbb4bc939
谭庆波 <tqb_20134342@163.com>
2018-11-13 11:04:30 +0800

    Update showimage.py

--- showimage.py
+++ showimage.py
@@ -9,41 +9,45 @@ import numpy as np
 import matplotlib.image as mpimg # mpimg 用于读取图片
 import matplotlib.pyplot as plt
 
-# img = Image.open(os.path.join('images', '2007_000648' + '.jpg'))
-# gray = img.convert('L')
-# r,g,b = img.split()
-# img_merged = Image.merge('RGB', (r, g, b))
+test_img_path = "E:/liverdata/nii/png/img/img0/"
+test_gt_path = "E:/liverdata/nii/png/label/label0/"
+test_pre_path="E:/liverdata/nii/png/label/label0/"
 
-test_img_path = "E:/liverdata/nii/png/img/img0/liver-orig1-89.png"
-test_gt_path = "E:/liverdata/nii/png/label/label0/liver-seg1-89.png"
-test_pre_path="E:/liverdata/nii/png/label/label0/liver-seg1-89.png"
+valid_img_path = "E:/liverdata/nii/png/img/img0/"
+valid_gt_path = "E:/liverdata/nii/png/label/label0/"
+valid_pre_path="E:/liverdata/nii/png/label/label0/"
 
-valid_img_path = "E:/liverdata/nii/png/img/img0/liver-orig1-89.png"
-valid_gt_path = "E:/liverdata/nii/png/label/label0/liver-seg1-89.png"
-valid_pre_path="E:/liverdata/nii/png/label/label0/liver-seg1-89.png"
+test_img_list = sorted(os.listdir(test_img_path))
+test_gt_list = sorted(os.listdir(test_gt_path))
+test_pre_list = sorted(os.listdir(test_pre_path))
+valid_img_list = sorted(os.listdir(valid_img_path))
+valid_gt_list = sorted(os.listdir(valid_gt_path))
+valid_pre_list = sorted(os.listdir(valid_pre_path))
 
-test_img = mpimg.imread(test_img_path)
-test_gt = mpimg.imread(test_gt_path)
-test_pre = mpimg.imread(test_pre_path)
+test_img_len = len(test_img_list)
 
-valid_img = mpimg.imread(valid_img_path)
-valid_gt = mpimg.imread(valid_gt_path)
-valid_pre = mpimg.imread(valid_pre_path)
+for i in range(test_img_len):
+    test_img = mpimg.imread(os.path.join(test_img_path, test_img_list[i]))
+    test_gt = mpimg.imread(os.path.join(test_gt_path, test_gt_list[i]))
+    test_pre = mpimg.imread(os.path.join(test_pre_path, test_pre_list[i]))
 
+    valid_img = mpimg.imread(os.path.join(valid_img_path, valid_img_list[i]))
+    valid_gt = mpimg.imread(os.path.join(valid_gt_path, valid_gt_list[i]))
+    valid_pre = mpimg.imread(os.path.join(valid_pre_path, valid_pre_list[i]))
 
-plt.figure(figsize=(10,5)) #设置窗口大小
-plt.suptitle('Multi_Image') # 图片名称
-plt.subplot(2,3,1), plt.title('test_img')
-plt.imshow(test_img,cmap='gray'), plt.axis('off')
-plt.subplot(2,3,2), plt.title('test_gt')
-plt.imshow(test_gt,cmap='gray'), plt.axis('off') #这里显示灰度图要加cmap
-plt.subplot(2,3,3), plt.title('test_pre')
-plt.imshow(test_pre,cmap='gray'), plt.axis('off')
-plt.subplot(2,3,4), plt.title('valid_img')
-plt.imshow(valid_img,cmap='gray'), plt.axis('off')
-plt.subplot(2,3,5), plt.title('valid_gt')
-plt.imshow(valid_gt,cmap='gray'), plt.axis('off')
-plt.subplot(2,3,6), plt.title('valid_pre')
-plt.imshow(valid_pre,cmap='gray'), plt.axis('off')
+    plt.figure(figsize=(10,5)) #设置窗口大小
+    plt.suptitle('Multi_Image') # 图片名称
+    plt.subplot(2,3,1), plt.title('test_img-'+test_img_list[i])
+    plt.imshow(test_img,cmap='gray'), plt.axis('off')
+    plt.subplot(2,3,2), plt.title('test_gt-'+test_gt_list[i])
+    plt.imshow(test_gt,cmap='gray'), plt.axis('off') #这里显示灰度图要加cmap
+    plt.subplot(2,3,3), plt.title('test_pre-'+test_pre_list[i])
+    plt.imshow(test_pre,cmap='gray'), plt.axis('off')
+    plt.subplot(2,3,4), plt.title('valid_img-'+valid_img_list[i])
+    plt.imshow(valid_img,cmap='gray'), plt.axis('off')
+    plt.subplot(2,3,5), plt.title('valid_gt-'+valid_gt_list[i])
+    plt.imshow(valid_gt,cmap='gray'), plt.axis('off')
+    plt.subplot(2,3,6), plt.title('valid_pre-'+valid_pre_list[i])
+    plt.imshow(valid_pre,cmap='gray'), plt.axis('off')
 
-plt.show()
+    plt.show()
